6e5311547b28c5bad1af78b1497ba6e282a572a9
jkzasori <jochitoel1@hotmail.com>
2018-03-22 02:11:55 -0500

    HTTP Requests with React Native

--- App.js
+++ App.js
@@ -8,20 +8,15 @@ import {
   View
 } from 'react-native';
 import Header from './src/components/header';
+import AlbumList from './src/components/AlbumList';
 
-const instructions = Platform.select({
-  ios: 'Press Cmd+R to reload,\n' +
-    'Cmd+D or shake for dev menu',
-  android: 'Double tap R on your keyboard to reload,\n' +
-    'Shake or press menu button for dev menu',
-});
 
-type Props = {};
-export default class App extends Component<Props> {
+export default class App extends Component{
   render() {
     return (
       <View style={styles.container}>
         <Header headerText='Albums!'/>
+        <AlbumList />
       </View>
     );
   }
